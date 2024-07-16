9e12e9a06261b8ecd2dde30c2629803720b3333a
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-07-16 13:23:34 +0300

    add table

--- webj/main/models.py
+++ webj/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField('Имя', max_length=20, default='default_name')
+    title = models.CharField('Пост', max_length=50)
+    short_decr = models.CharField('Краткое содержание', max_length=200)
+    text = models.TextField('Информация')
+    put_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
--- webj/main_1/views.py
+++ webj/main_1/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 def data(request):
+
     return render(request, 'main_1/data.html')
 
 def test(request):
-    return render(request, 'main_1/test.html')
+    posts = Post.objects.all()
+    return render(request, 'main_1/test.html', {'posts': posts})
+
